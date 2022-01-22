1bdd103753fbb20c9a3f5a44c0b6ffb0520bb565
aymaneltayary <ayman.eltayary@gmail.com>
2022-01-22 11:30:25 +0200

    define drone validation

--- drone-srv/src/main/java/com/musala/dto/DroneDto.java
+++ drone-srv/src/main/java/com/musala/dto/DroneDto.java
@@ -9,28 +9,40 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.Length;
 
-@Schema
+
 public class DroneDto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Length(max = 100)
+	@NotEmpty
+	@Size(max = 100, message = "serial number max length is 100")
 	private String serialNumber;
 
+	@NotNull(message = "weightLimit can not be null")
+	@Max(value = 500 , message ="max weight limit is 500gr" )
 	private Integer weightLimit;
 
+	@NotNull(message = "model can not be null")
 	private ModelEnum model;
 
+	@NotNull(message = "state can not be null")
 	private StateEnum state;
 
+	@NotNull(message = "battery can not be null")
 	private Integer battery;
 
 	private Integer createdBy;
 
 	private Integer modifiedBy;
 
-	private List<MedicationDto> cargo;
+	
 
 	public String getSerialNumber() {
 		return serialNumber;
@@ -88,19 +100,12 @@ public class DroneDto implements Serializable {
 		this.modifiedBy = modifiedBy;
 	}
 
-	public List<MedicationDto> getCargo() {
-		return cargo;
-	}
-
-	public void setCargo(List<MedicationDto> cargo) {
-		this.cargo = cargo;
-	}
-
 	@Override
 	public String toString() {
 		return "DroneDto [serialNumber=" + serialNumber + ", weightLimit=" + weightLimit + ", model=" + model
 				+ ", state=" + state + ", battery=" + battery + ", createdBy=" + createdBy + ", modifiedBy="
-				+ modifiedBy + ", cargo=" + cargo + "]";
+				+ modifiedBy + "]";
 	}
 
+	
 }
